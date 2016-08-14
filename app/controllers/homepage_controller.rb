4e4fd773c7e5da38a70691bd2f8475c9b2d7ad1b
StevenGreenup <greenup.steven@gmail.com>
2016-08-13 22:28:19 -0500

    added more contact info

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,20 @@
 class HomepageController < ApplicationController
-  def index
+  def homepage
+    @contact = Contact.new
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new
+    @contact.name = params[:contact][:name]
+    @contact.subject = params[:contact][:subject]
+    @contact.email = params[:contact][:email]
+    @contact.message = params[:contact][:message]
+     @contact.save
+      redirect_to root_path, notice:"Your message was sent. I will get back to you as soon as possible."
+
   end
 end
